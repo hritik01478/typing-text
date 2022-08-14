85c4d6ac42093bbee8f04dee66aeead2d08cf537
hritik01478 <hritikkumar01478@gmail.com>
2022-08-14 13:29:23 +0530

    result section made

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,6 +10,10 @@ const Home = () => {
     const SECONDS = 60;
     const [words, setWords] = useState([]);
     const [countDown, setCountDown] = useState(SECONDS);
+    const [currInput, setCurrInput] = useState("");
+    const [currWordIndex, setCurrWordIndex] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [incorrect, setIncorrect] = useState(0);
 
     useEffect(() => {
         setWords(randomWords(NUMBER_OF_WORDS))
@@ -27,11 +31,26 @@ const Home = () => {
             });
         }, 1000)
     }
-    // const generateWords = () => {
-    //     new Array(NUMBER_OF_WORDS).fill(null).map(() => randomWords());
-    // }
-    // console.log(words)
 
+    const handleKeyDown = ({ keyCode }) => {
+        if (keyCode === 32) {
+            checkMatch();
+            setCurrInput("")
+            setCurrWordIndex(currWordIndex + 1)
+        }
+    }
+
+    const checkMatch = () => {
+        const wordToCompare = words[currWordIndex];
+        const doesItMatch = wordToCompare === currInput.trim();
+        console.log({ doesItMatch })
+        if (doesItMatch) {
+            setCorrect(correct + 1);
+        }
+        else {
+            setIncorrect(incorrect + 1);
+        }
+    }
 
     return (
         <div className="typing">
@@ -39,20 +58,39 @@ const Home = () => {
                 <div className="typing-timer">{countDown}</div>
                 <div className="typing-card">
                     <div className="typing-content">
-                        {words.map((word) => (
-                            <>
-                                < span > {word}  </span>
-                            </>
+                        {words.map((word, i) => (
+                            <span key={i}>
+                                <span>
+                                    {word.split("").map((char, idx) => (
+                                        < span key={idx}>{char}</span>
+                                    ))}
+                                </span>
+                                <span> </span>
+                            </span>
                         ))}
                     </div>
                 </div>
                 <div className="input-card">
-                    <input type="text" className='input-content' placeholder='Type here...' />
+                    <input type="text" className='input-content' placeholder='Type here...' onKeyDown={handleKeyDown} value={currInput} onChange={(e) => setCurrInput(e.target.value)} />
                 </div>
                 <button className="typing-btn" onClick={handleTimer}>
                     START
                 </button>
             </div >
+            <div className="result-wrapper">
+                <div className="columns">
+                    <div className="wpm">
+                        <p>Words per minute : </p>
+                        <p className='result'>{correct}</p>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="wpm">
+                        <p>Accuracy : </p>
+                        <p className='result'>{Math.round((correct) / (correct + incorrect) * 100)} %</p>
+                    </div>
+                </div>
+            </div>
         </div >
     )
 }
